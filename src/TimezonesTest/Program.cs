70fe45c750f92d694d12de0cddfbae88a5f6b4d6
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2020-06-30 10:40:01 -0500

    Time zones
    First commit

---
+++ src/TimezonesTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TimeZoneConverter;
+
+namespace TimezonesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZoneInfo tzi = TZConvert.GetTimeZoneInfo("Central Standard Time");
+            TimeZoneInfo tzi2 = TZConvert.GetTimeZoneInfo("America/New_York");
+            Console.WriteLine(tzi);
+            Console.WriteLine(tzi2);
+        }
+    }
+}
